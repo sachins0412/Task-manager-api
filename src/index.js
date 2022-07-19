2e1248fca9a44dca085bcd575884dbc6820ff517
sachins0412 <saxenas964@gmail.com>
2022-07-19 22:32:12 +0530

    implement async/await for /tasks

--- src/index.js
+++ src/index.js
@@ -40,41 +40,43 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
-app.post("/task", (req, res) => {
+app.post("/task", async (req, res) => {
   const task = new Task(req.body);
-
-  task
-    .save()
-    .then(() => {
-      res.status(201).json(task);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+  try {
+    await task.save();
+    res.status(201).json(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+  // task
+  //   .save()
+  //   .then(() => {
+  //     res.status(201).json(task);
+  //   })
+  //   .catch((e) => {
+  //     res.status(400).send(e);
+  //   });
 });
 
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      res.json(tasks);
-    })
-    .catch((e) => {
-      res.status(500).send(e);
-    });
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.json(tasks);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
-app.get("/tasks/:id", (req, res) => {
-  const _id = req.params.id;
-  Task.findById(_id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send();
-      }
-      res.json(task);
-    })
-    .catch((e) => {
-      res.status(500).json(e);
-    });
+app.get("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.json(task);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 app.listen(PORT, () => {
