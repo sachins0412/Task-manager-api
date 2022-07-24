884474ac0f91c2b6d747553fe5586d7105c80968
sachins0412 <saxenas964@gmail.com>
2022-07-24 21:25:59 +0530

    add await

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,7 +4,10 @@ const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
     const decoded = jwt.verify(token, "secret");
-    const user = User.findOne({ _id: decoded._id, "tokens.token": token });
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
     if (!user) {
       throw new Error();
     }
