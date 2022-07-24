f05e117963e056938a35ecff5126e1c94f7dc8a6
sachins0412 <saxenas964@gmail.com>
2022-07-24 20:01:36 +0530

    implement JWTs signing

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = new express.Router();
-
 const User = require("../models/user");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(201).json(user);
+    const token = await user.generateAuthToken();
+    res.status(201).json({ user, token });
   } catch (e) {
     res.status(400).json(e);
   }
@@ -19,7 +19,8 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
-    res.json(user);
+    const token = await user.generateAuthToken();
+    res.json({ user, token });
   } catch (e) {
     res.status(400).send(e.message);
   }
