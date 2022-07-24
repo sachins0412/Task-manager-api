2e3ea0306051e7263c5631182540f81769fd8eec
sachins0412 <saxenas964@gmail.com>
2022-07-24 21:21:38 +0530

    implement JWT verification

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const User = require("../models/user");
+const auth = require("../middlewares/auth");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -26,7 +27,7 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
+router.get("/users", auth, async (req, res) => {
   try {
     const users = await User.find({});
     res.json(users);
