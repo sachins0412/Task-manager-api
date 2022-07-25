d6df576194192be0d123ee4b89808cab3d73c3d3
sachins0412 <saxenas964@gmail.com>
2022-07-25 23:33:37 +0530

    authenticate task endpoints & set up relationship with User

--- src/routers/task.js
+++ src/routers/task.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = new express.Router();
 
 const Task = require("../models/task");
+const auth = require("./../middlewares/auth");
 
-router.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
+router.post("/tasks", auth, async (req, res) => {
+  const task = new Task({
+    ...req.body,
+    owner: req.user._id,
+  });
   try {
     await task.save();
     res.status(201).json(task);
