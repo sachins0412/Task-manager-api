25a4a2b67b41515462cc2f65c62af44837f54681
sachins0412 <saxenas964@gmail.com>
2022-07-27 20:39:37 +0530

    adjust avatar size and format

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const router = new express.Router();
 const User = require("../models/user");
 const auth = require("../middlewares/auth");
 const multer = require("multer");
+const sharp = require("sharp");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -102,7 +103,11 @@ router.post(
   auth,
   upload.single("avatar"),
   async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
   },
@@ -119,7 +124,7 @@ router.get("/users/:id/avatar", async (req, res) => {
       throw new Error();
     }
 
-    res.set("Content-Type", "image/jpg");
+    res.set("Content-Type", "image/png");
     res.send(user.avatar);
   } catch (e) {
     res.status().send();
