1f6697c2e6e78fda4aea138c34a1208ab669babd
sachins0412 <saxenas964@gmail.com>
2022-07-27 19:39:32 +0530

    implement file uploads

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const User = require("../models/user");
 const auth = require("../middlewares/auth");
+const multer = require("multer");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -83,5 +84,29 @@ router.delete("/users/me", auth, async (req, res) => {
     res.status(500).send(e);
   }
 });
+
+const upload = multer({
+  dest: "avatars",
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|png|jpeg)$/)) {
+      cb(new Error("Unsupported format. Available formats : .jpg,.jpeg,.png"));
+    }
+    cb(undefined, true);
+  },
+});
+
+router.post(
+  "/users/me/avatar",
+  upload.single("avatar"),
+  (req, res) => {
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 module.exports = router;
