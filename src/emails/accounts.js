3861e630c524597f94a4ad230a45f625ee8e2452
sachins0412 <saxenas964@gmail.com>
2022-08-03 19:43:10 +0530

    set up emailing system

---
+++ src/emails/accounts.js
@@ -0,0 +1,41 @@
+const sgMail = require("@sendgrid/mail");
+const api_key = process.env.api_key;
+
+sgMail.setApiKey(api_key);
+
+const sendWelcomeEmail = (email, name) => {
+  sgMail
+    .send({
+      to: email,
+      from: "saxenas964@gmail.com",
+      subject: `Welcome to Task-App, ${name}!`,
+      text: "We hope you'll manage your tasks more effectly now !",
+    })
+    .then(() => {
+      console.log("mail sent for enrollment");
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+const sendCancellationEmail = (email, name) => {
+  sgMail
+    .send({
+      to: email,
+      from: "saxenas964@gmail.com",
+      subject: `GoodBye , ${name}!`,
+      text: "We are sorry to see you leave us. Would you spare a few mins please and let us what we could've done better ? !",
+    })
+    .then(() => {
+      console.log("mail sent for deletion");
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancellationEmail,
+};
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,11 +4,16 @@ const User = require("../models/user");
 const auth = require("../middlewares/auth");
 const multer = require("multer");
 const sharp = require("sharp");
+const {
+  sendWelcomeEmail,
+  sendCancellationEmail,
+} = require("./../emails/accounts");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
+    sendWelcomeEmail(user.email, user.name);
     const token = await user.generateAuthToken();
     res.status(201).json({ user, token });
   } catch (e) {
@@ -80,6 +85,7 @@ router.patch("/users/me", auth, async (req, res) => {
 router.delete("/users/me", auth, async (req, res) => {
   try {
     await req.user.remove();
+    sendCancellationEmail(req.user.email, req.user.name);
     res.json(req.user);
   } catch (e) {
     res.status(500).send(e);
